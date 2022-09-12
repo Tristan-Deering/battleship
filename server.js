a614f8044f4a90b88d94a262a1a6a21c37778b19
tristan deering <tristandeering@hotmail.com>
2022-09-12 12:43:56 -0600

    Initial project setup - navigation and styling

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ app.use(
   })
 );
 app.use(express.static('public'));
+app.use('/assets',express.static(path.join(__dirname, 'public/assets')));
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
